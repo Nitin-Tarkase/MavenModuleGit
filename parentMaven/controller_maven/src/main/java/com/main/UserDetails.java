e806903a4460084cbcec8f6da85e0c3ceca518f7
nitin <nitinblue98@gmail.com>
2018-03-19 22:39:29 +0530

    Initial Commit

---
+++ parentMaven/controller_maven/src/main/java/com/main/UserDetails.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import com.user.User;
+
+public class UserDetails {
+
+	public static void main(String[] args) {
+		User user=new User();
+		System.out.println(user.getName());
+
+	}
+
+}
---
+++ parentMaven/service_maven/src/main/java/com/user/User.java
@@ -0,0 +1,14 @@
+package com.user;
+
+public class User {
+ private String name="nitin";
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+ 
+}
